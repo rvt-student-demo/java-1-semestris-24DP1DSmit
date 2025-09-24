935f374361d9657ef6085af9016d602c18dd9a42
Denis <a240171ds@rvt.lv>
2025-09-24 10:09:16 +0300

    Tax

---
+++ src/main/java/rvt/gift.java
@@ -0,0 +1,27 @@
+package main.java.rvt;
+
+import java.util.Scanner;
+
+public class gift {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Value of the gift: ");
+        Integer gift = Integer.parseInt(scanner.nextLine());1
+
+        if (gift < 5000) {
+            System.out.println("No tax!");
+        } else if (gift < 25000) {
+            System.out.println(100 + (gift - 5000) * 0.08);
+        } else if (gift < 55000) {
+          System.out.println(1700 + (gift-55000) * 0.1);
+        } else if (gift < 200000) {
+            System.out.println(4700 + (gift - 200000) * 0.12);
+        } else if (gift < 1000000) {
+            System.out.println(22100 + (gift - 1000000) * 0.15);
+        } else {
+            System.out.println(142100 + (gift - 1000000) * 0.17); 
+        }
+
+    }
+}
