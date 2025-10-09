81b66241cee4693cbcaedf2d29047eb0bdfaa427
Denis <a240171ds@rvt.lv>
2025-10-09 12:18:31 +0300

    Advanced astrology

--- src/main/java/rvt/astrology.java
+++ src/main/java/rvt/astrology.java
@@ -1,20 +1,46 @@
 package main.java.rvt;
 
-import java.util.Scanner;
-
-public class astrology {
-    public static void main(String[] args) {
-        triangle(4);
-    }
-
-    public static void triangle(int number) {
+public class astrology{
+     public static void printSpaces(int number) {
         for (int i = 0; i < number; i++) {
-            
+            System.out.print(" ");
         }
     }
 
-    public static void cristmasTree(int x) {
-      
+    public static void printStars(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println(); 
+    }
 
+    public static void printTriangle(int size) {
+        for (int i = 1; i <= size; i++) {
+            printSpaces(size - i);
+            printStars(i);
+        }
+    }
+
+    public static void christmasTree(int height) {
+        for (int i = 1; i <= height; i++) {
+            printSpaces(height - i);
+            printStars(2 * i - 1);
+        }
+
+        for (int j = 0; j < 2; j++) {
+            printSpaces(height - 2);
+            printStars(3);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Triangle with size 4:");
+        printTriangle(4);
+
+        System.out.println("\nChristmas tree with height 4:");
+        christmasTree(4);
+
+        System.out.println("\nChristmas tree with height 10:");
+        christmasTree(10);
     }
 }
