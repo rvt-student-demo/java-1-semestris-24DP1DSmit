4422d08e31e1edbf85e7cc39eac8f833f9124981
Denis <a240171ds@rvt.lv>
2025-12-05 11:39:27 +0200

    ex1

--- src/main/java/rvt/Chapter44.java
+++ src/main/java/rvt/Chapter44.java
@@ -1,11 +1,23 @@
 package main.java.rvt;
+import java.util.Scanner;
 
 public class Chapter44 {
+    
     public static void main(String[] args) {
         
+        ex1();
     }
     public static void ex1() {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ievadiet vārdu: ");
+        String name = String.valueOf(scanner.nextLine());
+        scanner.close();
+        name = name.trim();
+        Integer prob = name.indexOf(" ", 1);
+        String vards = name.substring(0, prob);
+        String uzvards = name.substring(prob);
+        uzvards = uzvards.toUpperCase();
+        System.out.println(vards.concat(uzvards));
     }
     public static void ex2() {
         
